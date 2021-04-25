3cd3cf67f78cf893d7a62561ceb22f0737e7c67f
L1lPYTH0N <83186597+L1lPYTH0N@users.noreply.github.com>
2021-04-25 18:03:06 -0500

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,88 @@
+const help = (prefix) => {
+	return `
+「 *Python Bot V1.0* 」
+
+
+ ⌛ Estado Del Bot: Online
+
+
+◪ *Información*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Creador: L1lPyth0n
+  ❏ REGLAS
+  ❏ No Spam Al Bot ❌
+  ❏ No Agregarlo A Grupos Sin Permiso ❌
+  ❏ No Privado Al Bot ❌
+◪ *Para usar al bot*
+  ❏ Registrate con el comando ${prefix}daftar Nombre
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info (Info Del Bot)
+  ├─ ❏ ${prefix}blocklist (Lista De Usuarios Bloqueados)
+  ├─ ❏ ${prefix}chatlist (Lista De Chat)
+  ├─ ❏ ${prefix}ping (Velocidad Del Bot)
+  └─ ❏ ${prefix}bugreport (Reportar Bug)
+
+◪ *CREAR*
+  │
+  ├─ ❏ ${prefix}sticker (Hacer Stiker)
+  ├─ ❏ ${prefix}stickergif (Hacer Stiker Gif)
+  ├─ ❏ ${prefix}toimg (Sticker A JPG)
+  └─ ❏ ${prefix}tomp3 (Video A MP3)
+
+◪ *DESCARGAS*
+  │
+  └─ ❏ ${prefix}ytmp4 (Descargar Video De YouTube)
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play (Descargar Musica Nombre Y Artista) (A veces funciona)
+  ├─ ❏ ${prefix}bahasa (No funciona)
+  └─ ❏ ${prefix}tts (Texto A Voz)
+
+◪ *DUEÑO DEL BOT*
+  │
+  ├─ ❏ ${prefix}setprefix (Cambiar Prefijo)
+  ├─ ❏ ${prefix}block (Bloquear Usuário)
+  ├─ ❏ ${prefix}bc (Difusión A Todos Los Chats)
+  ├─ ❏ ${prefix}bcgc (Difusión A Todos Los Grupos)
+  └─ ❏ ${prefix}clearall (Borrar Todos)
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}wame (Link De Whatsapp)
+  ├─ ❏ ${prefix}virtex (Traba)
+  ├─ ❏ ${prefix}exe (. exe)
+  └─ ❏ ${prefix}qrcode (Crear QR)
+  
+◪ *GRUPOS*
+  │
+  ├─ ❏ ${prefix}ownergrup (Dueño Del Grupo)
+  ├─ ❏ ${prefix}closecg (Cerrar El Grupo Solo Admins)
+  ├─ ❏ ${prefix}opencg (Abrir Grupo Para Todos)
+  ├─ ❏ ${prefix}kick (Eliminar A Un Miembro)
+  ├─ ❏ ${prefix}promote (Dar Adm)
+  ├─ ❏ ${prefix}demote (Quitar Adm)
+  ├─ ❏ ${prefix}linkgc (Ver Link Del Grupo)
+  └─ ❏ ${prefix}tagall (Mencióna A Todos)
+
+Para usar estas funciones el bot necesita admin
+  
+◪ *NSWF*
+  │
+  └─ ❏ ${prefix}nsfwmenu (Menu De Nsfw)
+
+Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
+
+Redes sociales:
+
+https://www.facebook.com/Pinguino.Volador.Del.Futuro/
+https://www.instagram.com/lilpyth0n/
+https://www.twitch.tv/l1lpyth0n
+
+
+`
+}
+
+exports.help = help
